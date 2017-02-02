f8db0e9ca0bcf63a656fa6f2a73a90ed38845b3b
Alex Trevor <pascerveau@gmail.com>
2017-02-02 20:19:55 +0800

    Modified LFS

--- Source/TesingGrounds/TesingGroundsGameMode.cpp
+++ Source/TesingGrounds/TesingGroundsGameMode.cpp
@@ -9,7 +9,7 @@ ATesingGroundsGameMode::ATesingGroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/Blueprints/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
--- Source/TesingGrounds/TesingGroundsHUD.cpp
+++ Source/TesingGrounds/TesingGroundsHUD.cpp
@@ -9,7 +9,7 @@
 ATesingGroundsHUD::ATesingGroundsHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshiarTexObj(TEXT("/Game/Player/Textures/FirstPersonCrosshair.FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshiarTexObj(TEXT("/Game/Static/Player/Textures/FirstPersonCrosshair.FirstPersonCrosshair"));
 	CrosshairTex = CrosshiarTexObj.Object;
 }
 
